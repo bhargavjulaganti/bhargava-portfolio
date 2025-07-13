801d5fa74dd6e023a9bdda65631c27ff7375b43d
bhargava julaganti <bhargavjulaganti@gmail.com>
2025-07-13 11:17:43 -0400

    adding github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  assetPrefix: isProd ? "/bhargava-julaganti/" : undefined,
+  basePath: isProd ? "/bhargava-julaganti" : undefined,
 };
 
 export default nextConfig;
